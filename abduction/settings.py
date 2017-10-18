686dd61c409eb043739ae62d1c0b1f107f6748d3
Gabriel Lucchesi <lucchesidf@icloud.com>
2017-10-18 10:07:53 -0200

    updating time zone for America/Sao_Paulo

--- abduction/settings.py
+++ abduction/settings.py
@@ -103,15 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
