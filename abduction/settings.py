9b91f3d020fb79a01bb6da8853b636447a976ac2
Gabriel Lucchesi <lucchesidf@icloud.com>
2017-10-18 09:51:04 -0200

    Update settings.py

--- abduction/settings.py
+++ abduction/settings.py
@@ -76,16 +76,10 @@ WSGI_APPLICATION = 'abduction.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'abduction',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
